85016d9990b3ce766820df1579b9f4e35e1aea75
AnsheeJn <95086696+AnsheeJn@users.noreply.github.com>
2025-08-21 17:31:06 +0530

    Update code 2.0

--- ai-test-agent/app.py
+++ ai-test-agent/app.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 import io
 import time
@@ -125,10 +126,8 @@ if st.button("Generate Test Cases"):
             options.add_argument('--disable-dev-shm-usage')
 
             # Tell Selenium where to find the browser and driver
-            options.binary_location = "/usr/bin/chromium"
-            service = Service(executable_path="/usr/bin/chromedriver")
-            
-            driver = webdriver.Chrome(service=service, options=options)
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
             
             image_path = "full_page_screenshot.png"
             output_filename = "test_cases.txt"
